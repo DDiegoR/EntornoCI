e986c23e42aceba381b0e1b1292b890ad1e73af8
DDiegoR <diegoromerofrre@gmail.com>
2025-05-24 10:39:14 -0300

    fin

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #def suma(a,b):
     #return a + b
 import functions_framework
-from flask import jsonify, Flask
+from flask import request, jsonify, Flask
 
 app = Flask(__name__)
-def suma(a,b):
+
+@functions_framework.http
+def suma(request):
+    a = int(request.args.get("a", 0))
+    b = int(request.args.get("b", 0))
     return jsonify({"resultado": a + b})

a9096eea49a3ecf30f4a8930a49d7ba97cbff14a
DDiegoR <diegoromerofrre@gmail.com>
2025-05-24 10:43:47 -0300

    a verrrrrrrrrrrrrrr

--- test_main.py
+++ test_main.py
@@ -1,5 +1,14 @@
-from main import suma
+#from main import suma
 
-def test_suma () :
-    assert suma (3,3) == 6
-    assert suma (-1,1) == 0
+#def test_suma () :
+#    assert suma (3,3) == 6
+#    assert suma (-1,1) == 0
+
+
+from main import app, suma
+from flask import request
+
+def test_suma():
+    with app.test_request_context('/?a=2&b=3'):
+        response = suma(request)
+        assert response.get_json() == {"resultado": 5}
